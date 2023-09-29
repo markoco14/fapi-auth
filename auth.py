e9254fe56341c1084022df78a51a223caa4fd686
Mark O'Connor <mark.oconnor14@gmail.com>
2023-09-29 11:46:58 +0800

    feat: added login_for_access_token with authenticate_user and create_access_token methods

--- auth.py
+++ auth.py
@@ -5,7 +5,7 @@ from pydantic import BaseModel
 from sqlalchemy.orm import Session
 from starlette import status
 from database import SessionLocal
-from models import User
+import models
 from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from jose import jwt, JWTError
@@ -45,10 +45,40 @@ db_dependency = Annotated[Session, Depends(get_db)]
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
 def create_user(db: db_dependency, create_user_request: CreateUserRequest):
-    create_user_model = User(
+    create_user_model = models.User(
         email=create_user_request.email,
         hashed_password=bcrypt_context.hash(create_user_request.password)
     )
       
     db.add(create_user_model)
     db.commit()
+
+@router.post("/token", response_model=Token)
+def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
+    """
+    TODO: try to make an Email version
+    passing email in as username for email based auth
+    """
+    user = authenticate_user(email=form_data.username, password=form_data.password, db=db)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate user.")
+    token = create_access_token(email=user.email, user_id=user.id, expires_delta=timedelta(minutes=20))
+
+    return {"access_token": token, "token_type": "bearer"}
+
+def authenticate_user(email: str, password: str, db: db_dependency):
+    user: models.User = db.query(models.User).filter(models.User.email == email).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.hashed_password):
+        return False
+    
+    return user
+
+def create_access_token(email: str, user_id: int, expires_delta: timedelta):
+    encode = {"sub": email, "id": user_id}
+    expires = (datetime.utcnow() + expires_delta).isoformat()
+    encode.update({"expires": expires})
+
+    return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+   
