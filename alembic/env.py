d7181a1c234bc4a8ca976cbc553aead2320769b5
Mark O'Connor <mark.oconnor14@gmail.com>
2023-09-29 09:46:42 +0800

    feat: modify alembic files for mysql and url variables

--- alembic/env.py
+++ alembic/env.py
@@ -5,10 +5,15 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from database import DATABASE_URL, Base
+from models import User
+
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
 
+config.set_main_option('sqlalchemy.url', DATABASE_URL)
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
@@ -18,7 +23,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -38,7 +43,9 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    # config.set_main_option('sqlalchemy.url', DATABASE_URL)
+    # url = config.get_main_option("sqlalchemy.url")
+    url = DATABASE_URL
     context.configure(
         url=url,
         target_metadata=target_metadata,
