121e09c44f37163938f7bf1ace32bfa402fdbd5f
Mark O'Connor <mark.oconnor14@gmail.com>
2023-09-29 09:58:28 +0800

    feat: improve User model, migrate changes, update readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 SET UP
+
+```
+git clone
+pip install -r requirements.txt
+alembic revision --autogenerate -m "Create users table"
+alembic upgrade head
+uvicorn main:app --reload
+```
--- models.py
+++ models.py
@@ -1,9 +1,13 @@
-from sqlalchemy import Column, String, Text, Integer, Boolean
+from sqlalchemy import Column, String, Text, Integer, Boolean, DateTime, func
 from database import Base
 
 class User(Base):
 	__tablename__ = 'users'
 
 	id = Column(Integer, primary_key=True, index=True)
-	email = Column(String(length=None), unique=True)
-	hashed_password = Column(String(length=None))
+	email = Column(String(length=256), unique=True)
+	hashed_password = Column(String(length=256))
+	is_active = Column(Boolean, default=True)
+	is_verified = Column(Boolean, default=False)
+	created_at = Column(DateTime(timezone=True), server_default=func.now())
+	updated_at = Column(DateTime(timezone=True), onupdate=func.now())
