08db42ee10ae216b68c9989ea3597d6daed372d4
Mark O'Connor <mark.oconnor14@gmail.com>
2023-09-30 11:08:35 +0800

    feat: finalize create_access_token and create_refresh_token returns

--- core/security.py
+++ core/security.py
@@ -20,8 +20,8 @@ def create_access_token(data, expiry: timedelta):
 	payload = data.copy()
 	expire_in = datetime.utcnow() + expiry
 	payload.update({"exp": expire_in})
-	return jwt.encode(payload, settings.JWT_SECRET, algorithm=[settings.JWT_ALGORITHM])
+	return jwt.encode(payload, settings.JWT_SECRET, algorithm=settings.JWT_ALGORITHM)
 
 def create_refresh_token(data):
 	# Can add JWT_REFRESH_SECRET
-	return jwt.encode(data, settings.JWT_SECRET, algorithm=[settings.JWT_ALGORITHM])
+	return jwt.encode(data, settings.JWT_SECRET, algorithm=settings.JWT_ALGORITHM)
