2540f87ad86054e97c2811126014d648b50ee4fa
Mark O'Connor <mark.oconnor14@gmail.com>
2023-09-30 10:46:02 +0800

    feat: make create_access_token method in security.py

--- core/security.py
+++ core/security.py
@@ -1,5 +1,11 @@
+from datetime import datetime, timedelta
 from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordBearer
+from jose import jwt
+
+from core.config import get_settings
+
+settings = get_settings()
 
 pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/token")
@@ -9,3 +15,9 @@ def get_password_hash(password: str):
 
 def verify_password(plain_password: str, hashed_password: str):
 	return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data, expiry: timedelta):
+	payload = data.copy()
+	expire_in = datetime.utcnow() + expiry
+	payload.update({"exp": expire_in})
+	return jwt.encode(payload, settings.JWT_SECRET, algorithm=[settings.JWT_ALGORITHM])
