db105ee04426bc42cf5f62288cc2f10bcbc78500
Mark O'Connor <mark.oconnor14@gmail.com>
2023-09-30 08:46:57 +0800

    refactor: change to use get_settings() for DB_URL in database.py

--- database.py
+++ database.py
@@ -1,20 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
-from dotenv import load_dotenv
+from core import config
 
-load_dotenv()
+settings = config.get_settings()
 
-db_user = os.environ.get('DB_USER')
-db_password = os.environ.get('DB_PASSWORD')
-db_host = os.environ.get('DB_HOST')
-db_port = os.environ.get('DB_PORT')
-db_name = os.environ.get('DB_NAME')
-
-DATABASE_URL = f"mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
-
-engine = create_engine(DATABASE_URL)
+engine = create_engine(settings.DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
