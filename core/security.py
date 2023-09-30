7baccd7bd2cb0d91b17cf29794374b4cdb74930e
Mark O'Connor <mark.oconnor14@gmail.com>
2023-09-30 10:47:22 +0800

    feat: make basic create_refresh_token method in security.py

--- core/security.py
+++ core/security.py
@@ -21,3 +21,7 @@ def create_access_token(data, expiry: timedelta):
 	expire_in = datetime.utcnow() + expiry
 	payload.update({"exp": expire_in})
 	return jwt.encode(payload, settings.JWT_SECRET, algorithm=[settings.JWT_ALGORITHM])
+
+def create_refresh_token(data):
+	# Can add JWT_REFRESH_SECRET
+	return jwt.encode(data, settings.JWT_SECRET, algorithm=[settings.JWT_ALGORITHM])
