24cbf8522a1a9017e755b0c0b694f32a734aee9b
Mark O'Connor <mark.oconnor14@gmail.com>
2023-10-01 20:04:05 +0800

    feat: add expiry to refresh token

--- core/security.py
+++ core/security.py
@@ -29,9 +29,15 @@ def create_access_token(data, expiry: timedelta):
 	payload.update({"exp": expire_in})
 	return jwt.encode(payload, settings.JWT_SECRET, algorithm=settings.JWT_ALGORITHM)
 
-def create_refresh_token(data):
+"""
+creates a refresh token with default 3 day expiry
+"""
+def create_refresh_token(data, expiry: timedelta = timedelta(days=3)):
 	# Can add JWT_REFRESH_SECRET
-	return jwt.encode(data, settings.JWT_SECRET, algorithm=settings.JWT_ALGORITHM)
+	payload = data.copy()
+	expire_in = datetime.utcnow() + expiry
+	payload.update({"exp": expire_in})
+	return jwt.encode(payload, settings.JWT_SECRET, algorithm=settings.JWT_ALGORITHM)
 
 def get_token_payload(token: str):
 	try:
